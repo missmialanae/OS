b8fe079690b37cc3ebf9d5f8f1eab5a0e86f7aee
Mia Jenkins <you@example.com>
2020-09-22 22:41:52 -0400

    finished

--- Pandos/Phase1/asl.c
+++ Pandos/Phase1/asl.c
@@ -11,12 +11,13 @@ typedef struct semd_t {
 				   		 /*process queue */
 } semd_t; 
 
-HIDDEN pcb_t *pcbFree_h;
 
 /*semd_h points to active list*/
 /*semdFree_h points to free list*/
 
 HIDDEN semd_t *semd_h, *semdFree_h;
+
+HIDDEN semd_t* findsem(int *semAdd);
 
 void initASL(){
 	/*initialize the semdFree list to contain all the elements of the array*/ 
@@ -38,6 +39,8 @@ void initASL(){
 	semd_h->s_next->s_semAdd = MAXINT; 
 	semd_h->s_next->s_procQ = mkEmptyProcQ(); 
 	semd_h->s_next->s_next = NULL; 
+
+	return;
 	
 }
 
@@ -52,11 +55,10 @@ int insertBlocked(int *semAdd, pcb_t *p){
 
 	semd_t *temp = findsem(semAdd); /* finding the semaddress*/
 
-	debugB(1);
+	
 	if(temp->s_next->s_semAdd == semAdd){/* found the semAdd in the list */
 		p->p_semAdd = semAdd; 
-		insertProcQ(&(temp->s_procQ),p);
-		debugB(2);
+		insertProcQ(&(temp->s_next->s_procQ),p);
 		return FALSE; 
 	}
 
@@ -72,38 +74,30 @@ int insertBlocked(int *semAdd, pcb_t *p){
 	/*if the list is not empty and therefore I can use them*/
 
 	debugB(5);
-	if(semdFree_h != NULL){
 
-		/*so we remove from the top of the stack therefore we have to set the next asl to be the new pointer ot the head*/
+	/*so we remove from the top of the stack therefore we have to set the next asl to be the new pointer ot the head*/
+	semd_t *newSem = semdFree_h;
+	semdFree_h = newSem->s_next;
 
-		debugB(6);
-		semd_t *newSem = semdFree_h;
-		semdFree_h = newSem->s_next;
-		debugB(7);
-		/*this allows us to remove it from free list and allocate the stuff for it */
-		newSem->s_procQ = mkEmptyProcQ();
-		newSem->s_semAdd = semAdd; 
-		newSem->s_next = NULL;
+	/*this allows us to remove it from free list and allocate the stuff for it */
+	newSem->s_procQ = mkEmptyProcQ();
+	newSem->s_semAdd = semAdd; 
+	newSem->s_next = NULL;
 
-		/*how do we make sure it is on the active list*/
-		debugB(8);
-		temp = findsem(newSem->s_semAdd);
-		debugB(9);
-		newSem->s_next = temp->s_next;
-		debugB(10);
-		temp->s_next = newSem;
-		debugB(11);
-		p->p_semAdd = semAdd;
-		debugB(12);
-		insertProcQ(&(newSem->s_procQ),p);
+	/*how do we make sure it is on the active list*/
+	temp = findsem(newSem->s_semAdd);
+	newSem->s_next = temp->s_next;
+	temp->s_next = newSem;
+	p->p_semAdd = semAdd;
+	insertProcQ(&(newSem->s_procQ),p);
 
-		debugB(13);
-		return FALSE; 
-	}
+	
+	return FALSE; 
+	
 
 }
 
-pcb_t *removeBlocked(int *semAdd){
+pcb_t* removeBlocked(int *semAdd){
 	/*Search the ASL for a descriptor of this semaphore. If none is found, return NULL; otherwise, remove the head
 	pcb from the process queue of the found semaphore descriptor and return a pointer to it. If the process queue 
 	for this semaphore becomes empty (emptyProcQ(s procq) is TRUE), remove the semaphore descriptor from 
@@ -112,30 +106,29 @@ pcb_t *removeBlocked(int *semAdd){
 	/*search for the semAdd*/
 	semd_t *temp = findsem(semAdd);
 	if(temp->s_next->s_semAdd == semAdd){
-		removeProcQ(temp->s_next->s_procQ);
+		pcb_t *removedPCB = removeProcQ(&(temp->s_next->s_procQ));
+		if(emptyProcQ(temp->s_next->s_procQ) == TRUE){
 
-
-		if(emptyProcQ(temp->s_next->s_procQ)){
 			semd_t *remove = temp->s_next;
-			temp->s_next = remove->s_next;
+			temp->s_next = remove->s_next; 
 			remove->s_next = NULL;
 			remove->s_next = semdFree_h;
-			semdFree_h->s_next = remove; 
+			semdFree_h = remove;
 		}
 
-		return; 
+		return removedPCB; 
 	}
 	return NULL; 
 }
 
-pcb_t *outBlocked(pcb_t *p){
+pcb_t* outBlocked(pcb_t *p){
 	/* Remove the pcb pointed to by p from the process queue associated with p’s semaphore (p→ p semAdd) on the ASL. 
 	If pcb pointed to by p does not appear in the process queue associated with p’s semaphore, which is an error condition, r
 	eturn NULL; otherwise, re- turn p. */
 
 	semd_t *temp = findsem(p->p_semAdd);
 	if(temp->s_next->s_semAdd == p->p_semAdd){
-		outProcQ(temp->s_next->s_procQ, p);
+		pcb_t *outPCB = outProcQ(& (temp->s_next->s_procQ), p);
 
 		if(emptyProcQ(temp->s_next->s_procQ)){
 			semd_t *remove = temp->s_next;
@@ -144,29 +137,29 @@ pcb_t *outBlocked(pcb_t *p){
 			remove->s_next = semdFree_h;
 			semdFree_h->s_next = remove; 
 		}
-
-		return; 
+		return outPCB; 
 	}
-	return NULL; 
 
+	return NULL; 
 }
 
-pcb_t *headBlocked(int *semAdd){
+pcb_t* headBlocked(int *semAdd){
 	/* return to the pcb that is at the head of the process queue */
 
 	semd_t *temp = findsem(semAdd);/*creating a pcb to hold sem add*/ 
 
-	if(temp->s_next->s_semAdd != semAdd){
-		return NULL;
+	if(temp->s_next->s_semAdd == semAdd){
+		if(emptyProcQ(temp->s_next->s_procQ)){
+			return NULL;
+		}
+		return headProcQ(temp->s_next->s_procQ); 
 	}
-	if(emptyProcQ(temp->s_next->s_procQ)){
-		return NULL;
-	}
-	return headProcQ(temp->s_next->s_procQ); 
+
+	return NULL;
 
 }/* headblocked */
 
-int findsem(int *semAdd){
+semd_t* findsem(int *semAdd){
 	/* find the semAdd that is being called need. Basically a search function*/
 
 	semd_t *temp = semd_h;
