192016ffeca60e468b51b63d6400f4a675139b04
Mia Jenkins <you@example.com>
2020-10-23 02:44:14 -0400

    fixing interrupts

--- Pandos/Phase2/interrupts.c
+++ Pandos/Phase2/interrupts.c
@@ -35,50 +35,54 @@ int p; /* the local variable */
 
 void localTimer(cpu_t stopTOD){
 
-if(currentproc != NULL){
-	currentproc -> p_time = stopTOD - startTOD; /*change the time to the current process*/
+	if(currentproc != NULL){
+		currentproc -> p_time = stopTOD - startTOD; /*change the time to the current process*/
 
-	moveState(currentproc); /*store the processor state using move state*/
+		moveState(&BIOSDATAPAGE, &currentproc->p_s); /*store the processor state using move state*/
 
-	scheduler(); /*when its the end of processor's time (sliceCount), invoke the schedular*/
+		scheduler(); /*when its the end of processor's time (sliceCount), invoke the schedular*/
 
-	insertProc(readyqueue, currentproc); /* call insertProc*/
+		/*do we actually need to invoke the scheduler. Mikey says that and sometimes he be lyin*/
 
-	switch(); /*call switch */
-	/* also why is she red, do we not have switchProcess method?*/
+		insertProcQ(&readyqueue, currentproc); /* call insertProc*/
 
-}
+		procSwitch(); /*call switch */
 
-else if(currentProc = NULL){ /* if there is no current process running call panic*/
-	
-	PANIC();
-}
+	}
+
+	else if(currentProc = NULL){ /* if there is no current process running call panic*/
+		
+		PANIC();
+	}
 
 } /***END OF localTime****/
 
 
 void timerInt(){
 
-	/* ACK interrupt using LDIT */
+	/* ACK interrupt using LDIT; this is the pseudoclock tick */
+
+	pcb_t *p; 
+
+	LDIT(10000); /*need to put in the tume
 
 	semClock = 0; /*Wake everyone that is on wait, (p-clock)*/ 
 
-	removeBlocked(p); /*set p to removeBlocked*/
+	p = removeBlocked(); /*set p to removeBlocked*/
 
 	while(p != NULL){ /* while loop for when p isnt null*/
 
-	insertProc();
+		insertProcQ(&reaQueue, p);
 
-	softblock -= 1;
+		softblock -= 1;
+		p = removeBlocked(&semClock); 
 
-	p = removeBlocked(&semClock); 
+	}
 
-}
+	semClock = 0; //reset semaphore clock
 
-semClock = 0; //reset semaphore clock
-
-currentproc = NULL;
-switch();   /*it returns to the running process here*/
+	currentproc = NULL;
+	procSwitch();   /*it returns to the running process here*/
 
 
 
@@ -87,61 +91,61 @@ switch();   /*it returns to the running process here*/
 
 void IOHandler(int lineNum){
 
-/*deviceReg = RAMBASEADDR;*/    /*establish addressing here*/
+	deviceReg = RAMBASEADDR;    /*establish addressing here*/
 
-bitMap = deviceReg -> dev_interrupt[(lineInt - DISKINT)]; /* this is second part of addressing*/
+	bitMap = deviceReg -> interrupt_dev[(lineNum - DISKINT)]; /* this is second part of addressing*/
 
-/* While loop to determine the lowest number bit */
+	/* While loop to determine the lowest number bit */
 
-while(bitMap &DEVON != 0 ){
+	while(bitMap & DEVON != 0 ){
 
-if(DEV0ON <= DEV6ON){ /* if the bits are greater than 0 and equal 6 
+		if(DEV0ON <= DEV6ON){ /* if the bits are greater than 0 and equal 6 
 
-	/** HOW DO I DO THIS WHAT DO I SET THIS TO **/
+			/** HOW DO I DO THIS WHAT DO I SET THIS TO **/
 
-} else 
+		} else 
 
- deviceNum = 7; 
+		 deviceNum = 7; 
 
-
-}
-
-deviceSem = ((lineInt - DISKINT) * DEVPERINT) deviceNum; /*determine semaphore for device */
-
-/* special case for terminal interrupt-- idk if he added this or if we need it frfr? */
-
-if(intLine = TERMINT){
-
-intStat = timerInt(deviceSem);
-	
-/**more stuff goes in here **/
-
-} /*end of special case*/
-
-deviceSem[deviceSemNum] += 1; /* have to V the semaphore*/
-
-if(deviceSemNum <= 0){ /*waiting for the IO that is already performed here */
-
-	p = removeBlocked(&(deviceSem[deviceSemNum]));
-
-	if(p != NULL){
-
-		p -> p.s.s.v0 = intStat; /* save status in ACK */
-
-		insertBlocked();
-
-		softblock -=1;
 
 	}
 
-	devStatus [deviceSemNum] = intStat; /* save the status until syscall 8 */
+	deviceSem = ((lineNum - DISKINT) * DEVPERINT) * deviceNum; /*determine semaphore for device */
 
-	if(currentproc = NULL){ /**get a new proc **/
+	/* special case for terminal interrupt-- idk if he added this or if we need it frfr? */
 
-		switch();
+	if(intLine = TERMINT){
+
+		intStat = timerInt(deviceSem);
+			
+		/**more stuff goes in here **/
+
+	} /*end of special case*/
+
+	deviceSem[deviceSemNum] += 1; /* have to V the semaphore*/
+
+	if(deviceSemNum <= 0){ /*waiting for the IO that is already performed here */
+
+		p = removeBlocked(&(deviceSem[deviceSemNum]));
+
+		if(p != NULL){
+
+			p -> p.s.s.v0 = intStat; /* save status in ACK */
+
+			insertBlocked();
+
+			softblock -=1;
+
+		}
+
+		devStatus [deviceSemNum] = intStat; /* save the status until syscall 8 */
+
+		if(currentproc = NULL){ /**get a new proc **/
+
+			switch();
+		}
+
 	}
-
-}
 
 
 } /*****End of IOHandler*****/
